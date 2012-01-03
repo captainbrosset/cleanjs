69aa856c5aab5bb8f2ba8351699d7b90b446a666
Patrick Brosset <patrick.brosset@amadeus.com>
2012-01-03 23:30:29 +0100

    Added a check for camelcased function names

--- reviewers/naming.py
+++ reviewers/naming.py
@@ -3,6 +3,8 @@ import re
 from reviewers.base import BaseReviewer
 
 class Reviewer(BaseReviewer):
+	NB_OF_CHARS_IN_NAME_BEFORE_CAMELCASE = 10
+	
 	def review_gethasis_function_return(self, functions, message_bag):
 		for function in functions:
 			name = function.name
@@ -18,6 +20,16 @@ class Reviewer(BaseReviewer):
 			if is_set and len(function.signature) == 0:
 				message_bag.add_error(self, "Function " + name + " starts with 'set'. This usually means an argument is passed, but none was found.", function.line_nb);
 	
+	def review_camelcase_function_names(self, functions, message_bag):
+		for function in functions:
+			name = function.name
+			parts = re.sub("[A-Z]{1}", " ", name).split(" ")
+			if len(name) > Reviewer.NB_OF_CHARS_IN_NAME_BEFORE_CAMELCASE and len(parts) == 1:
+				message_bag.add_warning(self, "Function name " + name + " doesn't appear to be camelcase", function.line_nb)
+			
 	def review(self, file_data, message_bag):
 		self.review_gethasis_function_return(file_data.functions, message_bag)
 		self.review_set_function_arg(file_data.functions, message_bag)
+		self.review_camelcase_function_names(file_data.functions, message_bag)
+		# FIXME: implement this, should share the same code as the above method
+		#self.review_camelcase_variable_names(file_data.variables, message_bag)
