6f03626b76afc7fd1bb85155d8fe8914746ced99
Patrick Brosset <patrick.brosset@amadeus.com>
2012-01-03 18:44:15 +0100

    Added checks for comments after statements

--- reviewers/comments.py
+++ reviewers/comments.py
@@ -18,6 +18,12 @@ class Reviewer(BaseReviewer):
 			if ratio > Reviewer.MAX_CODE_COMMENT_RATIO_IN_FUNCTION:
 				message_bag.add_error(self, "There are more than " + str(int(Reviewer.MAX_CODE_COMMENT_RATIO_IN_FUNCTION*100)) + "% of comments in function " + function.name + " (" + str(ratio*100) + "%). Make the code simpler.");
 	
+	def review_comments_after_statements(self, content, message_bag):
+		comments = re.findall(";[\s]*//|\}[\s]*//", content)
+		if comments:
+			message_bag.add_warning(self, "It seems you have comments after statements or assignments <var a = 4; // some comment> or <if(...){...} // end if>. This is usually a sign that you need to explain a complex piece of code.")
+	
 	def review(self, file_data, message_bag):
 		self.review_multiple_comment_lines(file_data.content, message_bag)
 		self.review_comments_ratio_in_functions(file_data.functions, message_bag)
+		self.review_comments_after_statements(file_data.content, message_bag)
