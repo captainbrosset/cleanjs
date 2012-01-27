9b175bc45ec5f3b0035d7e5005dada25c4e9886c
Patrick Brosset <patrick.brosset@amadeus.com>
2012-01-27 13:39:14 +0100

    Removed the test scripts reports from git

--- tests/runtests.py
+++ tests/runtests.py
@@ -10,11 +10,19 @@ def run_unit_tests(dir):
 		except:
 			pass
 
+def ensure_dir(dir_name):
+	if not os.path.exists(dir_name):
+		os.makedirs(dir_name)
+
 def run_full_tests():
+	script_dir = "scripts"
+	report_dir = script_dir + os.sep + "reports"
+	ensure_dir(report_dir)
+	
 	for item in os.listdir("scripts"):
 		if item[-3:] == ".js":
 			print "-- RUNNING CLEANJS ON " + item
-			os.system("python ../cleanjs_cmdline.py scripts/" + item + " scripts/reports/" + item + "-report.html")
+			os.system("python .." + os.sep + "cleanjs_cmdline.py scripts" + os.sep + item + " " + report_dir + os.sep + item + "-report.html")
 
 print ""
 print "-- RUNNING ALL UNIT TETS"
